36195c79ae2d01c274dbc0ac0d91e7af62ea7640
Lasha <grdzelishvili.lasha@yahoo.com>
2024-02-13 19:34:05 +0400

    finished firs task

--- task_1.js
+++ task_1.js
@@ -11,3 +11,9 @@ Write a loop that makes seven calls to console.log to output the following trian
 ###### 
 #######
 */
+let a = ""
+let b = "#"
+for (let i = 0; i < 7; i++) {
+    console.log(a=a+b);
+  
+}
